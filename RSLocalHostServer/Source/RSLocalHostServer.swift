8ea287d0109a38e14013c093fb62cc769338b775
Ivan Tyurin <ivan.tyurin.job@gmail.com>
2021-10-13 17:01:19 +0300

    init commit

---
+++ RSLocalHostServer/Source/RSLocalHostServer.swift
@@ -0,0 +1,34 @@
+//
+//  RSLocalHostServer.swift
+//  POSUITests
+//
+//  Created by Ivan Tyurin on 13.10.2021.
+//
+
+import Foundation
+
+public class RSLocalHostServer {
+    let server: LocalhostServer
+    
+    public required init(portNumber: UInt) {
+        server = LocalhostServer(portNumber: portNumber)
+    }
+    
+    public func startListening() {
+        server.startListening()
+    }
+    
+    public func stopListening() {
+        server.stopListening()
+    }
+    
+    public func route(method: LocalhostRequestMethod,
+                      path: String,
+                      responseData: Data,
+                      statusCode: Int = 200,
+                      responseHeaderFields: [String: String]? = nil) {
+        server.route(method: method, path: path, responseData: responseData, statusCode: statusCode, responseHeaderFields: responseHeaderFields)
+    }
+    
+    
+}
---
+++ RSLocalhostServer.podspec
@@ -0,0 +1,17 @@
+Pod::Spec.new do |s|
+
+  s.name         = "RSLocalHostServer"
+  s.version      = "0.0.1"
+  s.summary      = "Locahost framework."
+  s.homepage     = "https://github.com/IvanTyurin/RSLocalHostServer.git"
+  s.license      = "MIT"
+  s.author       = { "Ivan Tyurin" => "ivan.tyurin@revelsystems.com" }
+  s.swift_version= '4.2'
+  s.platform     = :ios, "12.0"
+
+  s.source       = { :git => "https://github.com/IvanTyurin/RSLocalHostServer.git", :tag => s.version.to_s, :submodules => true }
+
+  s.source_files = 'RSLocalHostServer/RSLocalHostServer.h'
+  s.vendored_frameworks  = 'RSLocalHostServer.framework'
+  s.frameworks = 'Criollo', 'CocoaAsyncSocket'
+end
